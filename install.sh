133742752a072434a48dfc71dad022060bb27c80
newsworthy39 <newsworthy39@github.com>
2023-02-12 14:08:10 +0000

    Fix debian issues

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Configs related to running the control-plane
+POD_CIDR="10.244.0.0/16"
+APISERVER_IP=$(/usr/bin/ip address show dev enp0s3 | /usr/bin/grep -w inet | /usr/bin/awk '{print $2}' | /usr/bin/cut -d '/' -f 1)
+
+# Debian related workarounds.
+[ ! -d /etc/apt/keyrings ] &&  sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# netfilter
+sudo modprobe br_netfilter
+
+# Software needed.
+sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl
+
+# Keyring
+sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
+# add to apt repo
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+# Do a update
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl containerd.io
+sudo apt-mark hold kubelet kubeadm kubectl
+
+# ip_forward must be 1
+sudo sysctl -w net.ipv4.ip_forward=1
+echo "net.ipv4.ip_forward = 1" | sudo tee /etc/sysctl.d/20-ipforward.conf 
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+
+# Turn of swap
+sudo swapoff -a
+sudo systemctl enable containerd 
+sudo systemctl restart containerd 
+
+# init kubeadm
+sudo kubeadm init --pod-network-cidr=${POD_CIDR} --control-plane-endpoint=${APISERVER_IP}
