321de1006b94343c56fdec954455d890422a3a5d
newsworthy39 <newsworthy39@github.com>
2023-02-13 18:47:45 +0000

    Added dns-name

--- k8s-new-master.sh
+++ k8s-new-master.sh
@@ -4,9 +4,10 @@
 POD_CIDR="10.244.0.0/16"
 SERVICE_CIDR="10.96.0.0/12"
 APISERVER_IP=$(/usr/bin/ip address show dev enp0s3 | /usr/bin/grep -w inet | /usr/bin/awk '{print $2}' | /usr/bin/cut -d '/' -f 1)
+SERVICE_DNS_DOMAIN="cluster.local"
 
 # init kubeadm
-sudo kubeadm init --pod-network-cidr ${POD_CIDR} --control-plane-endpoint ${APISERVER_IP} --node-name "$(hostname)" --service-cidr ${SERVICE_CIDR}
+sudo kubeadm init --pod-network-cidr ${POD_CIDR} --control-plane-endpoint ${APISERVER_IP} --node-name "$(hostname)" --service-cidr ${SERVICE_CIDR} --service-dns-domain ${SERVICE_DNS_DOMAIN}
 
 # setup aliases
 cat <<EOF >> /home/${SUDO_USER}/.bash_aliases
