294217c9bc7e5b732b550b57852c734da1eedabd
newsworthy39 <newsworthy39@github.com>
2023-02-13 15:19:49 +0000

    Updated errors

--- k8s-master.sh
+++ k8s-master.sh
@@ -8,11 +8,6 @@ APISERVER_IP=$(/usr/bin/ip address show dev enp0s3 | /usr/bin/grep -w inet | /us
 # init kubeadm
 sudo kubeadm init --pod-network-cidr ${POD_CIDR} --control-plane-endpoint ${APISERVER_IP} --node-name "$(hostname)" --service-cidr ${SERVICE_CIDR}
 
-# setup kubectl
-mkdir -p /home/${SUDO_USER}/.kube
-sudo cp -fi /etc/kubernetes/admin.conf /home/${SUDO_USER}/.kube/config
-sudo chown $(id -u):$(id -g) /home/${SUDO_USER}/.kube/config
-
 # setup aliases
 cat <<EOF >> /home/${SUDO_USER}/.bash_aliases
 # set up autocomplete in bash into the current shell, bash-completion package should be installed first.
@@ -22,6 +17,10 @@ alias k=kubectl
 complete -o default -F __start_kubectl k
 EOF
 
+# setup kubectl
+mkdir -p /home/${SUDO_USER}/.kube
+sudo cp -fi /etc/kubernetes/admin.conf /home/${SUDO_USER}/.kube/config
+sudo chown -r ${SUDO_USER}:${SUDO_USER} /home/${SUDO_USER}/.kube /home/${SUDO_USER}/.bash_aliases
 
 # Add flannel to network.
 kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
