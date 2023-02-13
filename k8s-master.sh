ed3b0590a6d2124b5cd9ff0e3c985809c4fd3b89
newsworthy39 <newsworthy39@github.com>
2023-02-13 14:34:53 +0000

    Fixup

--- k8s-master.sh
+++ k8s-master.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# Configs related to running the control-plane
+# Configs related to running the control-plane. POD_CIDR is for internal-pod-ips. 
 POD_CIDR="10.244.0.0/16"
+SERVICE_CIDR="10.96.0.0/12"
 APISERVER_IP=$(/usr/bin/ip address show dev enp0s3 | /usr/bin/grep -w inet | /usr/bin/awk '{print $2}' | /usr/bin/cut -d '/' -f 1)
 
 # init kubeadm
-sudo kubeadm init --pod-network-cidr=${POD_CIDR} --control-plane-endpoint=${APISERVER_IP}
+sudo kubeadm init --pod-network-cidr ${POD_CIDR} --control-plane-endpoint ${APISERVER_IP} --node-name "$(hostname)-master" --service-cidr ${SERVICE_CIDR}
 
 # setup kubectl
 mkdir -p $HOME/.kube
